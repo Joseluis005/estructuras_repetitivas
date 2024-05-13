33ade93514ebb0599fc125b0bf5f04d325dc5c18
unknown <fernandezjoseluis2005@gmail.com>
2024-05-13 15:05:52 -0300

    tp finalizado

---
+++ assets/fizzbuzz.js
@@ -0,0 +1,12 @@
+for (let i=1; i<=15;i++){
+    if(i===3||i===6||i===9||i===12){
+        console.log("Fizz")
+    }
+    else if(i===5){
+        console.log("Buzz")
+    }
+    else if(i===15){
+        console.log("FizzBuzz");
+    }else{
+        console.log(i);}        
+}
---
+++ assets/numeros.js
@@ -0,0 +1,9 @@
+for (let i = 0; i <= 10; i++){
+    if (i===0){
+        console.log(i + "no es par ni impar")
+    }else if (i % 2 === 0) {
+        console.log(i + "es un número par");
+    }else{
+        console.log(i + "es un número impar")
+    }
+}
